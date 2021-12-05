4134e50ed4e96506bdfc2b510679bef4d71a3459
mahmoudramadan0040 <95087747+mahmoudramadan0040@users.noreply.github.com>
2021-12-05 02:31:35 +0200

    Update WebScrapingModel.py

--- WebScrapingModel.py
+++ WebScrapingModel.py
@@ -6,7 +6,8 @@ Created on Mon Nov 29 13:45:50 2021
 """
 
 #---- pakage for web scraping ----#
-import requests  
+ 
+import requests
 from bs4 import BeautifulSoup
 import csv 
 from itertools import zip_longest
@@ -15,14 +16,14 @@ from itertools import zip_longest
 
 
 # scraping all Books in this website "https://www.arab-books.com/"
-def ScrapingWebsite(Url,numWebsite=1):
-    i =1
+def ScrapingWebsite(Url,numWebsite):
+    
     authors =[]
     title_books=[]
     img_books=[]
     # scraping all website contains books 
     for i in range(numWebsite):
-        Data = requests.get(str(Url)+str(i))
+        Data = requests.get(str(Url)+str(i+1))
         #save page content/markup
         src =Data.content
         soup =BeautifulSoup(src,"lxml")
@@ -36,34 +37,34 @@ def ScrapingWebsite(Url,numWebsite=1):
     clean_img_books=[]
     clean_url_books=[]
     j=0
-    for j in range(len(title_books[0])):
-        clean_title_books.append(title_books[0][j].text)
-        clean_authors_name.append(authors[0][j].text)
-        clean_img_books.append(img_books[0][j].find("img").attrs['data-lazy-src'])
-        clean_url_books.append(title_books[0][j].find("a").attrs['href'])
+    for i in range(numWebsite):
+        for j in range(len(title_books[i])):
+            clean_title_books.append(title_books[i][j].text)
+            clean_authors_name.append(authors[i][j].text)
+            clean_img_books.append(img_books[i][j].find("img").attrs['data-lazy-src'])
+            clean_url_books.append(title_books[i][j].find("a").attrs['href'])
     
     # ---- Get Data inside A books website ---- #
     BooksInfo=[];
     PublishingHouse =[];
     TypeOfBook=[];
+    BooksDescription=[];
     i=0;
-    for i in range(len(title_books[0])):
+    
+    
+    for i in range(len(title_books[i])*numWebsite):
         InsideData = requests.get(clean_url_books[i]);
         src =InsideData.content;
         soup =BeautifulSoup(src,"lxml")
         BooksInfo.append(soup.find_all("div",{"class":"book-info"}))
-        # BooksDescription.append(soup.find_all("div",{"class":"entry-content entry clearfix"}))
-    # print(BooksInfo[1][0].find_all("li")[1].find("a").text)
-    # print(BooksInfo[1][0])
     i=0;
+    
     for i in range(len(BooksInfo)):
         PublishingHouse.append(BooksInfo[i][0].find_all("li")[4].text)
         TypeOfBook.append(BooksInfo[i][0].find_all("li")[1].text)
-    
-    
-    
+        
     #---- clean text from \n ----#
-    for k in range(len(title_books[0])):
+    for k in range(len(title_books[0])*numWebsite):
         clean_title_books[k]=clean_title_books[k].replace('PDF','');
         clean_title_books[k]=clean_title_books[k].replace('\n','');
         clean_authors_name[k]=clean_authors_name[k].replace("\n","");
@@ -86,12 +87,11 @@ def ScrapingWebsite(Url,numWebsite=1):
 def main():
     url = "https://www.arab-books.com//page/";
     
-    ScrapingWebsite(url,1) # change the number to the number of website in the link in range (1----> 198)
-
+    ScrapingWebsite(url,3) # change the number to the number of website in the link in range (1----> 198)
+    
+    
 if __name__ == "__main__":
     main()
-    
-    
     
     
     
