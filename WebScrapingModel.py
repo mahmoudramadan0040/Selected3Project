ade8556c543510e6227ee0a967813af6bb3fa08d
mahmoudramadan0040 <95087747+mahmoudramadan0040@users.noreply.github.com>
2021-12-14 13:45:05 +0200

    Update WebScrapingModel.py

--- WebScrapingModel.py
+++ WebScrapingModel.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 29 13:45:50 2021
-@author: mahmoud0020
-"""
-
 #---- pakage for web scraping ----#
  
 import requests
@@ -11,6 +5,7 @@ from bs4 import BeautifulSoup
 import csv 
 from itertools import zip_longest
 from ar_corrector.corrector import Corrector
+import re
 #---------------------------------#
 
 
@@ -50,6 +45,7 @@ def ScrapingWebsite(Url,numWebsite):
     PublishingHouse =[];
     TypeOfBook=[];
     BooksDescription=[];
+    
     i=0;
     
     
@@ -65,10 +61,21 @@ def ScrapingWebsite(Url,numWebsite):
     for i in range(len(BooksInfo)):
         PublishingHouse.append(BooksInfo[i][0].find_all("li")[4].text)
         TypeOfBook.append(BooksInfo[i][0].find_all("li")[1].text)
-        BookSummary.append(BooksDescription[i][0].find("a",attrs={'rel':'noopener'}).parent.text)
-        # BookSummary.append(BooksDescription[i][0].find("h2")[1])
-    # print(BooksDescription);
-    print(BookSummary);
+        tempString=BooksDescription[i][0].find("a",attrs={'rel':'noopener'}).parent.text
+        index=0;
+        while len(tempString)<230 and index < 5:
+            if len(tempString) > 230:
+                tempString=BooksDescription[i][0].find_all("p")[index].text
+                break;
+            index+=1;
+        secondIndex=0;
+        if len(tempString)< 50:
+            while len(tempString)<130 and secondIndex<8:    
+                tempString=BooksDescription[i][0].find_all("p")[secondIndex].text
+                secondIndex +=1
+        BookSummary.append(tempString)
+        
+    
     #---- clean text from \n ----#
     for k in range(len(title_books[0])*numWebsite):
         clean_title_books[k]=clean_title_books[k].replace('PDF','');
@@ -78,16 +85,60 @@ def ScrapingWebsite(Url,numWebsite):
         TypeOfBook[k]=TypeOfBook[k].replace("قسم الكتاب:",'')
         TypeOfBook[k]=TypeOfBook[k].replace("تحميل",'')
         TypeOfBook[k]=TypeOfBook[k].replace("\n",'')
-    
-    # ----- auto correct
+        TypeOfBook[k]=re.sub('^  كتب','',TypeOfBook[k])
+    # ------- split mutiTypeOfBook into multible rows -------#    
+    for i in range(30*numWebsite):
+        splitStr = len(re.findall('كتب',TypeOfBook[i]))
+        if(splitStr>=1):
+            multiTag=TypeOfBook[i].split('كتب')
+            if(len(multiTag[0])<3):
+                del multiTag[0]
+            for z in range(len(multiTag)):
+                TypeOfBook.append(multiTag[z])
+                clean_title_books.append(clean_title_books[i])                
+                clean_authors_name.append(clean_authors_name[i])                
+                clean_img_books.append(clean_img_books[i])                
+                clean_url_books.append(clean_url_books[i])                
+                BookSummary.append(BookSummary[i])                
+                PublishingHouse.append(PublishingHouse[i])
+    #----save the old rows that is used in spliting -------#
+    temp=[]
+    for i in range(len(TypeOfBook)):
+        splitStr = len(re.findall('كتب',TypeOfBook[i]))
+        if splitStr >=1:
+            temp.append(i)
+    # ----- delete old Rows --------#
+    for i in range(len(temp)):
+        del TypeOfBook[temp[i]]
+        del clean_title_books[temp[i]]
+        del clean_authors_name[temp[i]]
+        del clean_img_books[temp[i]]
+        del clean_url_books[temp[i]]
+        del BookSummary[temp[i]]
+        del PublishingHouse[temp[i]]
+        if ((i+1) % len(temp))==0:
+            break;
+        temp[(i + 1) % len(temp)]-=i+1
+        
+    # ----- auto correct ----------#
     corr = Corrector()
     for k in range(numWebsite*30):
         clean_title_books[k]=corr.contextual_correct(clean_title_books[k])
         clean_authors_name[k]=corr.contextual_correct(clean_authors_name[k])
         PublishingHouse[k]=corr.contextual_correct(PublishingHouse[k])
         TypeOfBook[k]=corr.contextual_correct(TypeOfBook[k])
-    # save data into exel sheet 
-    file_list =[clean_title_books,clean_authors_name,clean_url_books,clean_img_books,BookSummary,PublishingHouse,TypeOfBook]
+        
+
+            
+            
+    # ----------save data into exel sheets ------------#
+    file_list =[clean_title_books,
+                clean_authors_name,
+                clean_url_books,
+                clean_img_books,
+                BookSummary,
+                PublishingHouse,
+                TypeOfBook]
     
     compineList= zip_longest(*file_list,fillvalue=None)
     print(compineList)
@@ -99,12 +150,11 @@ def ScrapingWebsite(Url,numWebsite):
 def main():
     url = "https://www.arab-books.com//page/";
     
-    ScrapingWebsite(url,1) # change the number to the number of website in the link in range (1----> 198)
-    
+    ScrapingWebsite(url,3) # change the number to the number of website in the link in range (1----> 198)
+  
     
 if __name__ == "__main__":
     main()
-    
     
     
     
